bc5db8fc812b535c8d4773e02f47d471663eda6d
sh-navid <shamshirsaz.navid@live.com>
2021-03-17 14:21:25 +0330

    people

--- EinQSolver.py
+++ EinQSolver.py
@@ -1,3 +1,13 @@
 #By Navid Shamshirsaz
 
 #what we have at all???
+
+_people={
+    "1":"انگلیسی",
+    "2":"سوئدی",
+    "3":"دانکارکی",
+    "4":"نروژی",
+    "5":"آلمانی",
+    }
+
+print(_people)
