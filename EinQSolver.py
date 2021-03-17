9f2f997c1a409a4ac0090584c66edbcd7ede7168
sh-navid <shamshirsaz.navid@live.com>
2021-03-17 16:26:38 +0330

    update

--- EinQSolver.py
+++ EinQSolver.py
@@ -2,13 +2,13 @@
 
 import random
 #what we have at all???
-_0=["EN","SU","DN","NR","GR"]##human
-_1=["GREEN","WHITE","RED","BLUE","YELLOW"]##color of houses
-_2=["BIRD","HORSE","DOG","CAT","FISH"]##pet
-_3=["BEER","MILK","COFFEE","TEA","UNKNOWN"]##drink
-_4=["DH","PR","BL","BM","UNKNOWN2"]##sigaret
+_0 = ["EN", "SU", "DN", "NR", "GR"]  ##human
+_1 = ["GREEN", "WHITE", "RED", "BLUE", "YELLOW"]  ##color of houses
+_2 = ["BIRD", "HORSE", "DOG", "CAT", "FISH"]  ##pet
+_3 = ["BEER", "MILK", "COFFEE", "TEA", "UNKNOWN"]  ##drink
+_4 = ["DH", "PR", "BL", "BM", "UNKNOWN2"]  ##sigaret
 
-rulesA=[
+rulesA = [
     #MILK SHOULD BE IN CENTER HOUSE - DO THIS RULE SOMEWHERE ELSE
     #NR SHOULD N+BE IN FIRST HOUSE - DO THIS RULE SOMEWHERE ELSE
     "0_EN+1_RED",
@@ -21,38 +21,62 @@ rulesA=[
     "0_GR+4_PR",
 ]
 
-rulesB=[
-    "4_BL=LR|2_CAT",#The house of whom smokes BL is Left or Right side of House with a CAT
+rulesB = [
+    "4_BL=LR|2_CAT",  #The house of whom smokes BL is Left or Right side of House with a CAT
     "2_HORSE=LR|4_DH",
     "1_GREEN=L|1_WHITE",
     "0_GR=LR|1_BLUE",
     "4_BL=LR|3_BEER",
 ]
 
+
 #make a case
 def make():
-    solution=[
-        ["NR","","","",""],
-        ["","","","",""],
-        ["","","MILK","",""],
-        ["","","","",""],
-        ["","","","",""]
-    ]
+    solution = [["NR", "", "", "", ""], ["", "", "", "", ""],
+                ["", "", "MILK", "", ""], ["", "", "", "", ""],
+                ["", "", "", "", ""]]
     return solution
 
-s=make()
-for r in rulesA:
-    while True:
-        n=random.randint(0, 5)-1;#(1-5)-1 => (0-4)
-        lIdx=r.split("+")[0].split("_")[0]
-        lVal=r.split("+")[0].split("_")[1]
-        rIdx=r.split("+")[1].split("_")[0]
-        rVal=r.split("+")[1].split("_")[1]
-        if s[lIdx]=="" and s[rIdx]=="":
-            s[lIdx]=lVal
-            s[rIdx]=rVal
-            break
-    
-print(s);
+
+s = make()
 
 
+def firstRules():
+    s = make()
+    counter = 0
+    for i in range(0, 8):  #0-4
+        r = rulesA[i]
+        while True:
+            n = random.randint(0, 5) - 1
+            #(1-5)-1 => (0-4)
+            lIdx = int(r.split("+")[0].split("_")[0])
+            lVal = r.split("+")[0].split("_")[1]
+            rIdx = int(r.split("+")[1].split("_")[0])
+            rVal = r.split("+")[1].split("_")[1]
+            ss = s[n]
+            if ss[lIdx] == "" and ss[rIdx] == "":
+                ss[lIdx] = lVal
+                ss[rIdx] = rVal
+                break
+            else:
+                counter += 1
+                if (counter > 100):
+                    print(lIdx, lVal, rIdx, rVal)
+                    print("error")
+                    return False
+    return True
+
+
+while True:
+    ret = firstRules()
+    if ret:
+        break
+    pass
+
+for ss in s:
+    print(ss)
+
+counter = 0
+for i in range(0, 5):  #0-4
+
+    pass
