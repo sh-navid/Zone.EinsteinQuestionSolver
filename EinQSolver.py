8402cd16bab9503ad5b6c31de25b107dbe343378
sh-navid <shamshirsaz.navid@live.com>
2021-03-17 14:42:57 +0330

    update

--- EinQSolver.py
+++ EinQSolver.py
@@ -1,7 +1,6 @@
 #By Navid Shamshirsaz
 
 #what we have at all???
-
 _h=["EN","SU","DN","NR","GR"]##human
 
 _c=["GREEN","WHITE","RED","BLUE","YELLOW"]##color of houses
@@ -11,10 +10,6 @@ _p=["BIRD","HORSE","DOG","CAT","FISH"]##pet
 _d=["BEER","MILK","COFFEE","TEA","UNKNOWN"]##drink
 
 _s=["DH","PR","BL","BM","UNKNOWN2"]##sigaret
-
-print(_h)
-
-#Rules
 
 rules=[
     #MILK SHOULD BE IN CENTER HOUSE - DO THIS RULE SOMEWHERE ELSE
